28092959c0432e38ee24e02c1b2ac19ee2c5ac74
Karan Kumar <hellokaran7466@gmail.com>
2026-07-18 22:29:52 +0530

    Remove frontend from backend repository

--- LeetTrack-Frontend/src/services/authService.js
+++
@@ -1,9 +0,0 @@
-import api from "../api/axios";
-
-export const login = (data) => {
-    return api.post("/api/auth/login", data);
-};
-
-export const register = (data) => {
-    return api.post("/api/auth/register", data);
-};
